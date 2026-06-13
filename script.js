f9373d9fe76f30bf3eed9e36bab0d6c4bce46e44
Pupyukki <yukthasree.nareddy@gmail.com>
2026-06-13 15:42:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+let timer;
+let isrunning=false;
+let time=0;
+const clockdisplay = document.getElementById("myClockDisplay");
+const startstopbutton = document.getElementById("startStopButton");
+const resetbutton = document.getElementById("resetButton");
+startstopbutton.addEventListener('click',()=>{
+    if(isrunning){
+        clearInterval(timer);
+        startstopbutton.textContent="Start";
+    }
+    else{
+        timer=setInterval(updateTime, 10);
+        startstopbutton.textContent="Stop";
+    }
+    isrunning=!isrunning;
+});
+resetbutton.addEventListener('click',()=>{
+clearInterval(timer);
+time=0;
+isrunning=false;
+clockdisplay.textContent=formatTime(time);
+startstopbutton.textContent="Start";
+});
+function  updateTime(){
+    time+=10;
+    clockdisplay.textContent=formatTime(time);
+}
+function formatTime(ms){
+    const minutes=Math.floor(ms/60000);
+    const seconds=Math.floor((ms%60000)/1000);
+    const milliseconds=ms%1000;
+    `${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds,3)}`;
+}
+function padZero(num,length=2){
+    return num.toString().padStart(length,'0');
+}
